39fc47a5a2921bcf35c05af6f87b8e449e7bbd88
Ashiq Shovon <ashiqshovon39@gmail.com>
2020-02-26 09:12:53 +0600

    First commit

---
+++ controllers/home.js
@@ -0,0 +1,116 @@
+var express 	= require('express');
+var router 		= express.Router();
+var userModel   = require.main.require('./models/user-model');
+
+router.get('*', function(req, res, next){
+	if(req.cookies['username'] == null){
+		res.redirect('/login');
+	}else{
+		next();
+	}
+});
+
+router.get('/', function(req, res){	
+	if(req.cookies['username'] != null){
+		userModel.getByUname(req.cookies['username'], function(result){
+			
+			if(result.type == 'admin'){
+				res.render('home/index', {user: result});
+			}
+			
+
+		
+		});
+	}else{
+		res.redirect('/logout');
+	}
+});
+
+router.get('/alluser', function(req, res){
+	userModel.getAll(function(results){
+		if(results.length > 0){
+			res.render('home/alluser', {userlist: results});
+		}else{
+			res.send('invalid username/password');
+		}
+	});
+})
+
+
+router.get('/edit/:id', function(req, res){
+	
+	userModel.getById(req.params.id, function(result){
+		res.render('home/edit', {user: result});
+	});
+})
+
+router.post('/edit/:id', function(req, res){
+	
+	var user = {
+		username: req.body.username,
+		password: req.body.password,
+		type: req.body.type,
+		id: req.params.id
+	};
+
+	userModel.update(user, function(status){
+		if(status){
+			res.redirect('/home/alluser');
+		}else{
+			res.redirect('/home/edit/'+req.params.id);
+		}
+	});
+})
+
+//insert
+router.get('/insert', function(req, res){
+	
+	
+		res.render('home/insert');
+	
+})
+
+router.post('/insert', function(req, res){
+	
+	var user = {
+		username: req.body.username,
+		password: req.body.password,
+		type: req.body.type,
+		id: req.params.id
+	};
+
+	userModel.insert(user, function(status){
+		if(status){
+			res.redirect('/home/alluser');
+		}else{
+			res.redirect('/home/insert');
+		}
+	});
+})
+
+//delete
+
+
+router.get('/delete/:id', function(req, res){
+	
+	userModel.getById(req.params.id, function(result){
+		res.render('home/delete', {user: result});
+	});
+})
+
+router.post('/delete/:id', function(req, res){
+	
+	userModel.delete(req.params.id, function(status){
+		if(status){
+			res.redirect('/home/alluser');
+		}else{
+			res.redirect('/home/delete/'+req.params.id);
+		}
+	});
+	
+})
+
+
+
+module.exports = router;
+
